932126f448f81c2586ae0f97e19e489b3dfba31b
Aman Purohit <purohiam@sheridancollege.ca>
2025-03-22 23:54:07 -0400

    Add location, search functionality to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <div className="bg-white text-black">
     <h1> Hello Suckers;-)</h1>
+    <div>
+      <button> Enable location</button>
+      <input type="text" placeholder="Enter your location" />
+      <button> Search</button>
+    </div>
     </div>
   );
 }
